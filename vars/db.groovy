7ad5140e72e67a5bc1ec8bfbdb4a56dccd0edb2d
tqing1128 <tangqing@onemt.com.cn>
2023-08-22 09:52:10 +0800

    数据库

---
+++ vars/db.groovy
@@ -0,0 +1,20 @@
+#!groovy
+
+/*
+    author: TQING<tqing1128@gmail.com>
+    date: 2023-08-22 09:47:11
+*/
+
+package org.devops
+
+import groovy.json.JsonOutput
+import groovy.json.JsonSlurperClassic
+
+@groovy.transform.Field
+def data = []
+
+def write(key, value) {
+    data[key] = value
+    def json = JsonOutput.toJson(data)
+    writeFile file:"${env.JENKINS_DATA_DIR}/db.json", text:json
+}
