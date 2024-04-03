6e375c0f1ce4dc9c451f5473bf12e6f2e425c9d6
tqing1128 <tangqing@onemt.com.cn>
2024-04-03 10:53:47 +0800

    修复函数调用错误

--- vars/apollo.groovy
+++ vars/apollo.groovy
@@ -8,7 +8,12 @@
 import groovy.json.JsonOutput
 import groovy.json.JsonSlurperClassic
 
-private def requestApollo(method, url, token, data) {
+private def requestApollo(map) {
+    def method = map.method
+    def url = map.url
+    def token = map.token
+    def data = map.data
+
     println("requestApollo url: ${url}")
     def connection = new URL(url).openConnection();
     connection.setRequestMethod(method)
@@ -35,8 +40,47 @@ private def requestApollo(method, url, token, data) {
 
 def getClusterList(appId, host, token) {
     def url = "http://${host}/openapi/v1/apps/${appId}/envclusters"
-    def ret = requestApollo("GET", url, token)
-    return ret
+    return requestApollo method: "GET", url: url, token: token
+}
+
+def getCluster(map) {
+    def host = map.host
+    def appId = map.appId
+    def env = map.env
+    def cluster = map.cluster
+    def token = map.token
+
+    def url = "http://${host}/openapi/v1/envs/${env}/apps/${appId}/clusters/${cluster}"
+    return requestApollo method: "GET", url: url, token: token
+}
+
+def createCluster(map) {
+    def host = map.host
+    def appId = map.appId
+    def env = map.env
+    def token = map.token
+
+    def name = map.name
+    def user = map.user
+
+    def url = "http://${host}/openapi/v1/envs/${env}/apps/${appId}/clusters"
+    def data = [
+        name: name,
+        appId: appId,
+        dataChangeCreatedBy: user
+    ]
+    return requestApollo method: "POST", url: url, token: token, data: data
+}
+
+def getAllNamespace(map) {
+    def host = map.host
+    def appId = map.appId
+    def env = map.env
+    def token = map.token
+    def cluster = map.cluster
+
+    def url = "http://${host}/openapi/v1/envs/${env}/apps/${appId}/clusters/${cluster}/namespaces"
+    return requestApollo method: "GET", url: url, token: token
 }
 
 def getNamespace(map) {
@@ -48,5 +92,125 @@ def getNamespace(map) {
     def namespace = map.namespace
 
     def url = "http://${host}/openapi/v1/envs/${env}/apps/${appId}/clusters/${cluster}/namespaces/${namespace}"
-    return requestApollo("GET", url, token)
+    return requestApollo method: "GET", url: url, token: token
+}
+
+def createNamespace(map) {
+    def host = map.host
+    def appId = map.appId
+    def token = map.token
+
+    def name = map.name
+    def format = map.format
+    def isPublic = map.isPublic
+    def comment = map.comment
+    def user = map.user
+
+    def url = "http://${map.host}/openapi/v1/apps/${map.appId}/appnamespaces"
+
+    def data = [
+        name: map.name,
+        appId: map.appId,
+        format: map.format,
+        isPublic: map.isPublic,
+        comment: map.comment,
+        dataChangeCreatedBy: map.user
+    ]
+    return requestApollo method: "POST", url: url, token: token, data: data
+}
+
+def getItem(map) {
+    def host = map.host
+    def appId = map.appId
+    def env = map.env
+    def token = map.token
+    def cluster = map.cluster
+    def namespace = map.namespace
+    def key = map.key
+
+    def url = "http://${host}/openapi/v1/envs/${env}/apps/${appId}/clusters/${cluster}/namespaces/${namespace}/items/${key}"
+    return requestApollo method: "GET", url: url, token: token
+}
+
+def createItem(map) {
+    def host = map.host
+    def appId = map.appId
+    def env = map.env
+    def token = map.token
+    def cluster = map.cluster
+    def namespace = map.namespace
+
+    def key = map.key
+    def value = map.value
+    def comment = map.comment
+    def user = map.user
+
+    def url = "http://${host}/openapi/v1/envs/${env}/apps/${appId}/clusters/${cluster}/namespaces/${namespace}/items"
+    def data = [
+        key: key,
+        value: value,
+        comment: comment,
+        dataChangeCreatedBy: user
+    ]
+    return requestApollo method: "POST", url: url, token: token, data: data
+}
+
+def updateItem(map) {
+    def host = map.host
+    def appId = map.appId
+    def env = map.env
+    def token = map.token
+    def cluster = map.cluster
+    def namespace = map.namespace
+
+    def key = map.key
+    def value = map.value
+    def comment = map.comment
+    def user = map.user
+    def createIfNotExists = map.createIfNotExists or true
+
+    def url = "http://${host}/openapi/v1/envs/${env}/apps/${appId}/clusters/${cluster}/namespaces/${namespace}/items/${key}?createIfNotExists=${createIfNotExists}"
+
+    def data = [
+        key: key,
+        value: value,
+        comment: comment,
+        dataChangeLastModifiedBy: user,
+        dataChangeCreatedBy: user
+    ]
+    return requestApollo method: "PUT", url: url, token: token, data: data
+}
+
+def deleteItem(map) {
+    def host = map.host
+    def appId = map.appId
+    def env = map.env
+    def token = map.token
+    def cluster = map.cluster
+    def namespace = map.namespace
+    def key = map.key
+    def user = map.user
+
+    def url = "http://${host}/openapi/v1/envs/${env}/apps/${appId}/clusters/${cluster}/namespaces/${namespace}/items/${key}?operator=${user}"
+    return requestApollo method: "DELETE", url: url, token: token
+}
+
+def releaseNamespace(map) {
+    def host = map.host
+    def appId = map.appId
+    def env = map.env
+    def token = map.token
+    def cluster = map.cluster
+    def namespace = map.namespace
+    def title = map.title
+    def comment = map.comment
+    def user = map.user
+
+    def url = "http://${host}/openapi/v1/envs/${env}/apps/${appId}/clusters/${cluster}/namespaces/${namespace}/releases"
+    def data = [
+        releaseTitle: title,
+        releaseComment: comment,
+        releasedBy: user
+    ]
+    return requestApollo method: "POST", url: url, token: token, data: data
 }
